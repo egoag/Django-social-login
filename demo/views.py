ff788f31446286c91d831993abbcab23206cd7ac
youyaochi <youyaochihaha@qq.com>
2015-06-18 01:03:54 +0900

    add avatar in nav

--- demo/views.py
+++ demo/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth import logout as auth_logout
 from django.http import HttpResponseRedirect
 
+
 def index(request):
-    return render(request,'index.html',{})
+    return render(request, 'index.html',{'user':request.user})
+
 
 def logout(request):
     auth_logout(request)
     return HttpResponseRedirect('/')
-
